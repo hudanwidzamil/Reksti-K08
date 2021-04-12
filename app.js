05d16f356a3a0ac148acb0acc2ff1f79e0a1ab8c
naufalalimw <52844969+naufalalimw@users.noreply.github.com>
2021-04-12 17:57:45 +0700

    initiate backend

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
+// const swaggerUi = require('swagger-ui-express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird')
+mongoose.set('useFindAndModify', false)
+require('dotenv').config()
+
+
+//mongo connect
+mongoose.connect('mongodb+srv://naufalalimw:nalimw12@cluster0.kv1bh.mongodb.net/reksti08?retryWrites=true&w=majority').then(
+      db => {
+        console.log('Connected to db server')
+        console.log('Connection state: ' + mongoose.connection.readyState)
+      },
+      err => {
+        console.error(err)
+      }
+    )
+    .catch(err => {
+      console.error(err.message)
+    });
+
+
+
+const saldoRoutes = require('./routes/saldo.routes')
+// App
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.raw())
+app.use(cookieParser())
+app.use(cors())
+
+app.use(saldoRoutes)
+
+module.exports = app;
